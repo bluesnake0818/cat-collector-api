6a8eb87ea84c566c4c26851d473fd52e60a03082
bluesnake0818 <raidient87@gmail.com>
2022-04-08 12:50:50 -0400

    add create cate, index cat

--- api/__init__.py
+++ api/__init__.py
@@ -11,6 +11,7 @@ from api.models.cat import Cat
 
 # ============ Import Views ============
 from api.views.auth import auth
+from api.views.cats import cats
 
 list = ['GET', 'HEAD', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE', 'LINK']
 cors = CORS()
@@ -26,7 +27,7 @@ def create_app(config):
 
   # ============ Register Blueprints ============
   app.register_blueprint(auth, url_prefix='/api/auth') 
-  # app.register_blueprint(cats, url_prefix='/api/cats') 
+  app.register_blueprint(cats, url_prefix='/api/cats') 
   # app.register_blueprint(toys, url_prefix='/api/toys') 
 
   return app
